ba35a179d119b29f13974b3bcfc556b0762b152c
hazem <hazem.benkhalfallah@ncq-devstudio.com>
2023-10-09 11:39:57 +0100

    add a log when updating a value in cache

--- src/main/java/com/vsware/libraries/redisreactive/cache/aspect/AbstractReactiveCacheAspect.java
+++ src/main/java/com/vsware/libraries/redisreactive/cache/aspect/AbstractReactiveCacheAspect.java
@@ -1,6 +1,7 @@
 package com.vsware.libraries.redisreactive.cache.aspect;
 
 import com.vsware.libraries.redisreactive.cache.ports.CachePort;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -8,26 +9,27 @@ import reactor.core.publisher.Mono;
 /**
  * @author hazem
  */
+@Slf4j
 abstract class AbstractReactiveCacheAspect {
 
     abstract CachePort getCache();
 
     protected Mono<?> putMethodMonoResponseToCache(ProceedingJoinPoint joinPoint, String key) {
         return proceedAsMono(joinPoint)
-                .map(methodResponse -> {
-                    getCache().set(key, methodResponse).subscribe();
-                    return methodResponse;
-                });
+                .map(methodResponse -> updateCache(key, methodResponse));
     }
 
     protected Flux<?> putMethodFluxResponseToCache(ProceedingJoinPoint joinPoint, String key) {
         return proceedAsFlux(joinPoint)
                 .collectList()
-                .map(methodResponseList -> {
-                    getCache().set(key, methodResponseList).subscribe();
-                    return methodResponseList;
-                })
+                .map(methodResponseList -> updateCache(key, methodResponseList))
                 .flatMapMany(Flux::fromIterable);
+    }
+
+    private <T> T updateCache(String key, T valueToCache) {
+        log.info("Put key [{}] in cache", key);
+        getCache().set(key, valueToCache).subscribe();
+        return valueToCache;
     }
 
     protected Mono<?> proceedAsMono(ProceedingJoinPoint joinPoint) {
